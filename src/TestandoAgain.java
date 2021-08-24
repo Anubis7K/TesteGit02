713ac8b78ad34bee4be84d0e760b234427769c1a
LucasBoni29 <santosboni2002@gmail.com>
2021-08-24 17:06:39 -0300

    Adicionando classe com variaveis, multiplicação e soma

---
+++ src/TestandoAgain.java
@@ -0,0 +1,17 @@
+
+public class TestandoAgain {
+
+	public static void main(String[] args) {
+		System.out.print("Atualizando no git");
+		
+		int num1 = 8;
+		int num2 = 10;
+		int num3 = 9;
+		int num4 = 5;
+		
+		System.out.println("O resultado da soma é: "+num1+num2);
+		System.out.println("O resultado da multiplicação é: "+num3*num4);
+
+	}
+
+}
